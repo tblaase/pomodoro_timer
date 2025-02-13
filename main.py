dc38d83e49238776c3f4c2d49ef5530ea395aa19
tblaase <85116019+tblaase@users.noreply.github.com>
2025-02-13 15:05:54 +0100

    added buttons and labels

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import tkinter as tk
+from tkinter import ttk
+from tkinter.constants import DISABLED, NORMAL
 
 # ---------------------------- CONSTANTS ------------------------------- #
 PINK = "#e2979c"
@@ -22,11 +24,24 @@ LONG_BREAK_MIN = 20
 window = tk.Tk()
 window.title("Pomodoro")
 window.config(padx=100, pady=50, bg=YELLOW)
+
+title_label = tk.Label(text="Timer", fg=GREEN, font=(FONT_NAME, 50, "bold"), bg=YELLOW)
+title_label.grid(column=1,row=0)
+
 canvas = tk.Canvas(width=200, height=224, bg=YELLOW, highlightthickness=0)
 tomato_img = tk.PhotoImage(file="tomato.png")
 canvas.create_image(100, 112, image=tomato_img)
 timer_text = canvas.create_text(100, 130, text="00:00", fill="black", font=(FONT_NAME, 35, "bold"))
 canvas.grid(column=1,row=1)
+
+start_button = ttk.Button(window, text="Start", command=start_timer)
+start_button.grid(column=0, row=2)
+reset_button = ttk.Button(window, text="Reset", command=reset_timer, state=DISABLED)
+reset_button.grid(column=2, row=2)
+
+check_marks = tk.Label(fg=GREEN, bg=YELLOW, font=(FONT_NAME,20))
+check_marks.grid(column=1, row=3)
+
 
 # global variables
 window.mainloop()
